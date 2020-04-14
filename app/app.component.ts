726cad74cfe58accb121f7a4b313d050dc079739
gianlucacastelnovo <gianlucacastelnovo@gmail.com>
2020-04-14 11:58:28 +0200

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -18,4 +18,20 @@ export class AppComponent  {
       });
     }
   }
+
+
+ getLocation(event){
+    let offsetLeft = 0;
+    let offsetTop = 0;
+
+    let el = event.srcElement;
+
+    while(el){
+        offsetLeft += el.offsetLeft;
+        offsetTop += el.offsetTop;
+        el = el.parentElement;
+    }
+    return { offsetTop:offsetTop , offsetLeft:offsetLeft }
+}
+
 }
--- app/hello.component.ts
+++ app/hello.component.ts
@@ -7,4 +7,20 @@ import { Component, Input } from '@angular/core';
 })
 export class HelloComponent  {
   @Input() name: string;
+
+
+
+  getLocation(event){
+    let offsetLeft = 0;
+    let offsetTop = 0;
+
+    let el = event.srcElement;
+
+    while(el){
+        offsetLeft += el.offsetLeft;
+        offsetTop += el.offsetTop;
+        el = el.parentElement;
+    }
+    return { offsetTop:offsetTop , offsetLeft:offsetLeft }
+}
 }
